9442637a5c85c809755ceff0545022c17db9264d
Kacper Grabiec <grabiec.kacper01@gmail.com>
2024-07-13 19:53:56 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# **Weather App**
+
+An iOS weather app.
---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Kacper Grabiec on 13/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
